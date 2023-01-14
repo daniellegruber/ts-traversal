0447d48ef8116e979e73ba5787b25808335d6744
Danielle Gruber <dlg59@c14n02.grace.hpc.yale.internal>
2023-01-14 10:52:15 -0500

    generatedCode_test new makefile

---
+++ generatedCode_test/external_fun/myfun3.c
@@ -0,0 +1,14 @@
+//Link
+#include <stdio.h>
+#include <stdbool.h>
+#include <complex.h>
+#include <string.h>
+#include <matrix.h>
+#include <myfun3.h>
+
+
+int myfun3(int a) {
+int mat1 = pow(a, 2);
+int b = mat1;
+return b;
+}
